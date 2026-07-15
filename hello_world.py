10cab2ccfa47c30de45db144df7894da2caab993
panduisback <paneendra183286@gmail.com>
2026-07-15 17:12:38 +0530

    adding a new python file

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+class Student:
+     def _init_(stu):
+       stu.name = "bhargav"
+       stu.id   = 47856
+       stu.sub  = "b.com"
+
+     def talk(stu):
+       print('my name is', stu.name)
+       print('this id', stu.id)
+       print('our subject', stu.sub)
+
+       s1 = student()
+       s1.talk()
+       print(s1.name)
+       print(s2.id)
+       print(s3.sub)
+
+    
